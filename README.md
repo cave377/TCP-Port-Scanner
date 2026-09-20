0dfc30ac945f141e022361491fa537c13255f969
cave377 <kevin.vieira2@hotmail.com>
2026-09-20 11:47:26 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Escaneamento de portas com Go
+
+## Escopo
+Um escaner de portas com possibilidade de compilação para diversos sistemas operacionais
+
+## Instalação/Compilação
+### Linux
+```
+GOOS="linux" GOARCH"amd64" go build main.go
+```
+### Windows
+```
+GOOS="windows" GOARCH"amd64" go build main.go
+```
+### MAC
+```
+GOOS="darwin" GOARCH"ard64" go build main.go
+```
+
+## Utilização
+
+![alt text](images/image-4.png)
+
+Há quatro tipos de utilização:
+
+*Obs.:* Os exemplos serão dados sem compilação
+
+### Porta única
+```
+go run main.go -u localhost -p 443
+```
+Resultado:
+
+![alt text](images/image.png)
+
+### Range de portas
+```
+go run main.go -u localhost -p 100-1000
+```
+Resultado:
+
+![alt text](images/image-1.png)
+
+### Portas específicas
+```
+go run main.go -u localhost -p 135,445,1000
+```
+Resultado:
+
+![alt text](images/image-2.png)
+
+### Modo default
+
+O Modo Default utiliza por padrão a varredura das portas de 1 à 1000
+
+```
+go run main.go -u localhost
+```
+Resultado:
+
+![alt text](images/image-3.png)
+
+
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func varredura(inicio int, fim int, alvo string) {
+	for door := inicio; door <= fim; door++ {
+		address := fmt.Sprintf("%s:%d", alvo, door)
+		_, err := net.Dial("tcp", address)
+		if err == nil {
+			fmt.Println("Porta aberta: ", door)
+		}
+	}
+}
+
+func main() {
+	alvo := flag.String("u", "", "url do alvo")
+	porta := flag.String("p", "default", "porta para escaneamento (default escaneia da 1-1000)")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Uso: main.go [opções]\n\n")
+		fmt.Fprintln(os.Stderr, "\nOpções:")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "\nExemplos:")
+		fmt.Fprintf(os.Stderr, "-u http://www.example.com -p 22\n")
+		fmt.Fprintf(os.Stderr, "-u http://www.example.com -p 22-100\n")
+		fmt.Fprintf(os.Stderr, "-u http://www.example.com -p 22,53,135\n")
+	}
+	flag.Parse()
+	if *alvo == "" {
+		fmt.Fprintf(os.Stderr, "erro: a flag -u é obrigatória \n")
+		flag.Usage()
+	} else {
+		fmt.Println("Alvo -> ", *alvo)
+		switch {
+		case strings.Contains(*porta, "-"):
+			fmt.Println("Portas a verificar -> ", *porta)
+			tratado := strings.Split(*porta, "-")
+			i, _ := strconv.Atoi(tratado[0])
+			f, _ := strconv.Atoi(tratado[1])
+			varredura(i, f, *alvo)
+
+		case strings.Contains(*porta, ","):
+			fmt.Println("Portas a verificar -> ", *porta)
+			tratado := strings.Split(*porta, ",")
+			for cont := 0; cont < len(tratado); cont++ {
+				door, _ := strconv.Atoi(tratado[cont])
+				address := fmt.Sprintf("%s:%d", *alvo, door)
+				_, err := net.Dial("tcp", address)
+				if err == nil {
+					fmt.Println("Porta aberta: ", tratado[cont])
+				}
+			}
+		case *porta == "default":
+			fmt.Println("Portas a verificar -> ", *porta)
+			varredura(1, 1000, *alvo)
+		case !strings.ContainsAny(*porta, "-,"):
+			fmt.Println("Portas a verificar -> ", *porta)
+			tratado, _ := strconv.Atoi(*porta)
+			address := fmt.Sprintf("%s:%d", *alvo, tratado)
+			_, err := net.Dial("tcp", address)
+			if err == nil {
+				fmt.Println("Porta aberta: ", tratado)
+			}
+		default:
+			flag.Usage()
+		}
+	}
+	fmt.Println("\n\nSaindo...")
+}
